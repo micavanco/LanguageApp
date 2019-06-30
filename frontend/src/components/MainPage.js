3d85840c1b149b0312ecefb95187e3e5de52f838
micavanco <micavanco@gmail.com>
2019-06-30 22:05:13 +0200

    Rearranged whole project by adding separated frontend application

---
+++ frontend/src/components/MainPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './MainPage.css';
+
+export default class MainPage extends Component{
+
+    render()
+    {
+        return(
+            <div className={"main-container"}>
+
+            </div>
+        );
+    }
+}
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './NavBar.css';
+
+export default class NavBar extends Component{
+
+    render()
+    {
+        return(
+            <div className={"nav-container"}>
+                <div className={"logo"}>OLECH</div>
+                <div className={"nav-btn"}>Login</div>
+                <div className={"nav-btn"}>Best Score</div>
+            </div>
+        );
+    }
+}
